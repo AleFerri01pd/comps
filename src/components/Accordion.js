1f419d4a7aad1d38fa5d93fc686d0d0478148d92
AleFerri01pd <ferri.alessandro2001@gmail.com>
2023-11-25 18:41:58 +0100

    added styling, collapse and icons

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,37 @@
 import { useState } from "react";
+import { VscChevronDown, VscChevronRight } from "react-icons/vsc";
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(0);
+    const [expandedIndex, setExpandedIndex] = useState(-1);
     
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
-        const content = isExpanded && <div>{item.content}</div>; // && prende in considerazione il primo valore valso o il secondo vero
+        const content = isExpanded && <div className="delay-150 border-b p-5">{item.content}</div>; // && prende in considerazione il primo valore valso o il secondo vero
+        const icon =  
+            <span className="text-xl">
+                { isExpanded ? <VscChevronDown /> : <VscChevronRight />}
+            </span>
+
+        const handleClick = (nextIndex) => {
+            if(expandedIndex === nextIndex)
+                setExpandedIndex(-1)
+            else 
+                setExpandedIndex(nextIndex)
+        }
 
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+                <div onClick={() => handleClick(index)} className="flex p-3 bg-gray-300 border-b items-center justify-between cursor-pointer">
+                    {item.label}
+                    {icon}
+                </div>
                 {content}
             </div>
         )
     });
 
     return (
-        <div>
+        <div className="border-x border-t rounded">
             {renderedItems}
         </div>
     )
