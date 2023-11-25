55f672278d94f2c8784cfb0f1f5b54df905f2c94
AleFerri01pd <ferri.alessandro2001@gmail.com>
2023-11-25 18:18:27 +0100

    setup app for Accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Accordion from "./components/Accordion";
+
 function App() {
+	const items = [
+		{
+			id: Math.floor(Math.random() * 1000000),
+			label: "Item 1",
+			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae sem nibh. Phasellus varius nisl augue, a aliquet ipsum sollicitudin nec. Sed imperdiet et lacus hendrerit placerat."
+		},
+		{
+			id: Math.floor(Math.random() * 1000000),
+			label: "Item 2",
+			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+		},
+		{
+			id: Math.floor(Math.random() * 1000000),
+			label: "Item 3",
+			content: "Consectetur adipiscing elit. Fusce vitae sem nibh. Phasellus varius nisl augue, a aliquet ipsum sollicitudin nec. Sed imperdiet et lacus hendrerit placerat."
+		},
+	]
 	return (
-		<div>
-		</div>
+		<Accordion items={items} />
 	)
 }
 
