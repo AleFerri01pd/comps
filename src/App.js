643295385169b7bb3f30753163416cb8aa9006e1
AleFerri01pd <ferri.alessandro2001@gmail.com>
2023-11-27 17:36:31 +0100

    reset App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import Accordion from "./components/Accordion";
-
 function App() {
-	const items = [
-		{
-			id: Math.floor(Math.random() * 1000000),
-			label: "Item 1",
-			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae sem nibh. Phasellus varius nisl augue, a aliquet ipsum sollicitudin nec. Sed imperdiet et lacus hendrerit placerat."
-		},
-		{
-			id: Math.floor(Math.random() * 1000000),
-			label: "Item 2",
-			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-		},
-		{
-			id: Math.floor(Math.random() * 1000000),
-			label: "Item 3",
-			content: "Consectetur adipiscing elit. Fusce vitae sem nibh. Phasellus varius nisl augue, a aliquet ipsum sollicitudin nec. Sed imperdiet et lacus hendrerit placerat."
-		},
-	]
-	return (
-		<Accordion items={items} />
-	)
+	return <div></div>
 }
 
 export default App;
